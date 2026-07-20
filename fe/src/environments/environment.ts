9265c6737e3235db8e778105d2cca706328c6200
StormyGit <dani.cruzlopez2002@gmail.com>
2026-07-20 12:10:38 -0600

    hotfix:

---
+++ fe/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  serve_incidenteApplication: 'http://localhost:8081/api/bomberosHonduras/Incidentes',
+  serve_usersApplication: 'http://localhost:8082/api/bomberosHonduras/Users',
+  keySecret: 'mi_clave_super_secreta_de_32_caracteres_minimo_123456'
+};
